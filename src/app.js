811be15e6ad6666ced7f0574d3caa13ba340c820
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-15 15:35:58 +0000

    first

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  //Declaramos las arrays
+  let element = ["corazon", "trebol", "pica", "diamante"];
+  let number = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  //Variables Random
+  let randomElements = Math.floor(Math.random() * elements.length);
+  let randomNumbers = Math.floor(Math.random() * numbers.length);
+
+  //Accedemos a los elementos
+  let top = document.querySelector(".top");
+  let numbers = document.querySelector(".numbers");
+  let bottom = document.querySelector(".bottom");
+
+  //Manipulacion
+  numbers.innerHTML = number[randomNumbers];
 };
